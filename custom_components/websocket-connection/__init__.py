108fc48757de6843d829864fe0e9c571afd5f133
Jonathan Treffler <trefflerj@web.de>
2020-05-02 14:38:53 +0000

    Update custom_components/websocket-connection/__init__.py, custom_components/websocket-connection/services.yaml files

--- custom_components/websocket-connection/__init__.py
+++ custom_components/websocket-connection/__init__.py
@@ -3,10 +3,10 @@ DOMAIN = "websocket_connection"
 def setup(hass, config):
     """Set up is called when Home Assistant is loading our component."""
 
-    def handle_request(host, message):
+    def handle_send(host, message):
         """Handle the service call."""
 
-    hass.services.register(DOMAIN, "send", handle_hello)
+    hass.services.register(DOMAIN, "send", handle_send)
 
     # Return boolean to indicate that initialization was successfully.
     return True
