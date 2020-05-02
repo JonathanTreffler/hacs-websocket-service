9fb719280f791992385cd4c2109ba50c4554085a
Jonathan Treffler <trefflerj@web.de>
2020-05-02 15:10:02 +0000

    Update custom_components/websocket-connection/__init__.py

--- custom_components/websocket-connection/__init__.py
+++ custom_components/websocket-connection/__init__.py
@@ -3,8 +3,11 @@ DOMAIN = "websocket_connection"
 def setup(hass, config):
     """Set up is called when Home Assistant is loading our component."""
 
-    def handle_send(host, message):
+    def handle_send(call):
         """Handle the service call."""
+        host = call.data["host"]
+        message = call.data["message"]
+
         uri = "ws://"+host
         async with websockets.connect(uri) as websocket:
             await websocket.send(input(message))
